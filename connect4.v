6cdffdf3f98c80d03e5fe513bda069a1302c2efd
Vince Wu <vincewu01@hotmail.com>
2019-03-11 10:34:26 -0700

    added display stuff to connect4.v

--- connect4.v
+++ connect4.v
@@ -27,4 +27,56 @@ end
 
 // display stuff
 
+integer pixelCol;
+integer pixelRow;
+
+always @(posedge clk) begin
+    pixelCol <= (PixelX - 120) / 70;
+    pixelRow <= (PixelY - 60) / 60;
+
+    if (PixelX < 110 || PixelX > 530 || PixelY < 60 || PixelY > 420) begin
+        //blue border
+        R = 3'b000;
+        G = 3'b101;
+        B = 2'b11;
+    else if (gameStatePIECE[pixelCol][7-pixelRow] == 1) begin  //draw square pieces
+        // Check piece color
+        if (gameStateSIDE[pixelCol][7-pixelRow] == BLACK) begin
+            // yellow piece
+            R = 3'b111;
+            G = 3'b111;
+            B = 2'b00;
+            end
+        else begin
+            // red piece
+            R = 3'b111;
+            G = 3'b000;
+            B = 2'b00;
+            end
+        end
+    else if (pixelCol % 2 == pixelRow % 2) begin
+        // Yellow checkerboard squares
+        R = 3'b111;
+        G = 3'b111;
+        B = 2'b00;
+        // Highlight column if selected
+        if (pixelCol == currColumn) begin
+            R = 3'b111;
+            G = 3'b111;
+            B = 2'b11;
+            end
+        end
+    else begin
+        // Blue default checkerboard
+        R = 3'b000;
+        G = 3'b000;
+        B = 2'b11;
+        // Highlight column if selected
+        if (pixelCol == currColumn) begin
+            R = 3'b111;
+            G = 3'b111;
+            B = 2'b11;
+            end
+        end
+    end
 endmodule
