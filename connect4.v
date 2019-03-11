c18610469fc6290312b6aefabc7093623b7011c1
MacBook Air <macbookair@MacBooks-MacBook-Air-2.local>
2019-03-11 10:39:29 -0700

    game logic

--- connect4.v
+++ connect4.v
@@ -1,4 +1,4 @@
-module connect4(
+/**module connect4(
     input clock,
 
     input [10:0] x_pixel,
@@ -12,17 +12,92 @@ module connect4(
     output reg [2:0] vgaR,
     output reg [2:0] vgaG,
     output reg [1:0] vgaB
-);
+);**/
 
 
 // initialize game state
+module connect4(
+    input myClk,
+    input [10:0] x_pixel,
+    input [10:0] y_pixel,
+    input [2:0] col,
+    input pop,
+    input reset, //resetGame
 
+    input reg [3:0] keypadButton,
 
-always @(*)
-begin
-    if (resetGame)
+   // input resetGame,
+    input resetScore,
+
+    output reg [2:0] vgaR,
+    output reg [2:0] vgaG,
+    output reg [1:0] vgaB
+);
+
+//0: Yellow, 1: Red
+
+reg player; 
+player <= 1; // red first
+
+reg [6:0] piece [5:0];
+reg [6:0] state [5:0];
+reg [6:0] top; // saves how tall column is for every column (from 1 - 6 tall, 0 = none)
+
+reg [3:0] x;
+reg [3:0] y;
+
+initial begin
+    for(x = 0; x < 7; x = x + 1) begin
+        top[x] = 0;
+        for(y = 0; y < 6; y = y + 1) begin  
+            piece[x][y] <= 0;
+            state[x][y] <= 0;    
+        end
+    end
 end
 
+always @(posedge clk) begin
+    if(reset) begin
+        for(x = 0; x < 7; x = x + 1) begin
+            top[x] = 0;
+            for(y = 0; y < 6; y = y + 1) begin  
+                piece[x][y] <= 0;
+                state[x][y] <= 0;    
+                player <= 1;
+            end
+        end
+    end
+    else 
+        if(pop)begin
+            if(col >= 0 && col < 111)begin
+                if(state[col][0] == player) begin
+                    for(y = 0; y < top[col] - 1; y = y + 1) begin
+                        state[col] = state[col +1];
+                    end
+                    state[col][top[col] -1] = 0;
+                    piece[col][top[col] -1] = 0;
+                    top[col] = top[col] -1;
+                    player = ~player;
+                end
+                else 
+                    //choose again
+            end
+            else    
+                //choose again
+        end
+        else if( col >= 0 && col < 111 ) begin
+            if(top[col] < 110 ) begin
+                piece[col][top[col] + 1 ] <= 1;
+                state[col][top[col] + 1 ] <= player;
+                player <= ~player
+            end
+            else 
+            //choose again
+        end    
+        else //choose again
+end
+
+}
 
 
 // display stuff
